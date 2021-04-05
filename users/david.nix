e6b7494eff94c7b00012f876407b9a079cbc46ae
Duarte David <deltaduartedavid@gmail.com>
2021-04-05 18:40:55 +0200

    Add ssh key for david

--- users/david.nix
+++ users/david.nix
@@ -13,6 +13,9 @@ in {
     home = "/home/david";
     extraGroups = [ "wheel" "networkmanager" "docker" "vboxusers" "dialout" ];
     shell = pkgs.unstable.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDowHs9CSn76pFw8l5p0uI27hiZB/ZuV1aEeVrDoqjRvNFAs9jy6KzHk8E85+VckfaDv+u2ZQA4NtJHW+PIntrbeCz06soTspMqFUbZ5ombR8ywwlJhKsS0aDwtNpPHhvzv3A1+Vkbp1pI4p40IT5li48WBDQ4f5UTW81WK96XZpw2dW7RDiWNDoH848nBbhE+NY7TcmlI1czQPbuAsH0Sl/nYxEoTUU/I7UwBIcZpvVl/qs3uE4qSaaWR9O+LXKnvpr/F5RMTcIEg5q7qK442rOPn2o+9Qwwm7sttTLYIFyrZa+wZYMjzB/FWGHIqtWb4lZZEJSEvR+ji6u1FpCiBZ deltaduartedavid@gmail.com"
+    ];
   };
 
   programs.fish.enable = true;
